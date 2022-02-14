7e57edaa71fa7dc87f26007b51b87c4a7bfc73fc
LeoGim1997 <gimenezleo4@outlook.fr>
2022-02-14 23:10:26 +0000

    make gaussian blur working for all  size kernel

--- filter.py
+++ filter.py
@@ -1,37 +1,38 @@
-from function import gaussian_kernel
+from function import gaussian_kernel, gaussian_kernel_basis
 import numpy as np
 
-def gaussian_blur(img : np.array ) -> np.array:
+def gaussian_blur(img : np.array, sigma :float = 1 ,fill_pad = False) -> np.array:
     '''
     Apply a 7x7 gaussian blur on a img (default)
     on 1 channel of the input image
     '''
-    filter = gaussian_kernel(1)
+    filter = gaussian_kernel(sigma)
     n,m = np.shape(img)
+    cut = np.shape(filter)
+    half_witdh = cut[0]//2
     #pad the image for fitting
-    img_c = np.ones((n+3,m+3))
-    img_c[3:,3:] = img
+    img_c = np.zeros((n+half_witdh,m+half_witdh))
+    img_c[half_witdh:,half_witdh:] = img
     #filling the 4 corner with the same corner value as the original image
+    if fill_pad:
+        #top block of the matrix
+        img_c[0:half_witdh,0:half_witdh] = img[0,0]*img_c[0:half_witdh,0:half_witdh]
+        for i in range(half_witdh):
+            img_c[i,half_witdh:-half_witdh] = img[0,:-half_witdh]
+        img_c[0:half_witdh,-half_witdh:] = img[0,-1]*img_c[0:half_witdh,-half_witdh:]
+        #bottom block of the matrix
+        img_c[-half_witdh:,0:half_witdh] = img[-1,0]*img_c[-half_witdh:,0:half_witdh]
+        for i in range(1,half_witdh):
+            img_c[-i,half_witdh:-half_witdh] = img[-1,:-half_witdh]
+        img_c[-half_witdh:,-half_witdh:] = img[-1,-1]*img_c[-half_witdh:,-half_witdh:]
 
-    #top block of the matrix
-    img_c[0:3,0:3] = img[0,0]*img_c[0:3,0:3]
-    for i in range(3):
-        img_c[i,3:-3] = img[0,:-3]
-    img_c[0:3,-3:] = img[0,-1]*img_c[0:3,-3:]
-    #bottom block of the matrix
-    img_c[-3:,0:3] = img[-1,0]*img_c[-3:,0:3]
-    for i in range(1,3):
-        img_c[-i,3:-3] = img[-1,:-3]
-    img_c[-3:,-3:] = img[-1,-1]*img_c[-3:,-3:]
+        #left and right block of the matrix
+        img_c[half_witdh:,0:half_witdh] = img[:,0:half_witdh]
+        img_c[half_witdh:,-half_witdh:] = img[:,-half_witdh:]
 
-    #left and right block of the matrix
-    img_c[1:-2,0:3] = img[:,0:3]
-    img_c[1:-2,-3:] = img[:,-3:]
-
-    #lower-left corner and bottom row
-    for i in range(3,n-4):
-        for j in range(3,m-4):
-                sub_matrix = img[i-3:i+4,j-3:j+4]
+    for i in range(half_witdh,n-half_witdh):
+        for j in range(half_witdh,m-half_witdh-1):
+                sub_matrix = img_c[i-half_witdh:i+half_witdh+1,j-half_witdh:j+half_witdh+1]
                 prod = np.multiply(sub_matrix,filter)
                 img_c[i,j] = np.average(prod)
-    return img_c[3:-4,4:-4]
+    return img_c
