2504229dd518efab8cbf7b6aa988d1c12b42a341
LeoGim1997 <gimenezleo4@outlook.fr>
2022-02-15 13:16:06 +0000

    add padding function in filter.py

--- filter.py
+++ filter.py
@@ -11,8 +11,27 @@ def gaussian_blur(img : np.array, sigma :float = 1 ,fill_pad = False) -> np.arra
     cut = np.shape(filter)
     hw = cut[0]//2
     #pad the image for fitting
+    img_c = image_padding(img,hw,fill_pad)
+    N,M = np.shape(img_c)
+    for i in range(hw,N-hw):
+        for j in range(hw,M-hw-1):
+                sub_matrix = img_c[i-hw:i+hw+1,j-hw:j+hw+1]
+                prod = np.multiply(sub_matrix,filter)
+                img_c[i,j] = np.average(prod)
+    return img_c[hw:-hw,hw:-hw]
+
+
+def image_padding(img : np.array , half_pad :int = 4,fill_pad =False) -> np.array:
+    '''
+    Function returning a padded image use for convolution
+    with a square fitler of half_with = half_pad
+    '''
+    n,m = np.shape(img)
+    hw = half_pad
+    #pad the image for fitting
     img_c = np.ones((n+2*hw,m+2*hw))
     img_c[hw:-hw,hw:-hw] = img
+
     #filling the 4 corner with the same corner value as the original image
     if fill_pad:
         #top block of the matrix
@@ -23,16 +42,10 @@ def gaussian_blur(img : np.array, sigma :float = 1 ,fill_pad = False) -> np.arra
         #bottom block of the matrix
         img_c[-hw:,0:hw] = img[-1,0]*img_c[-hw:,0:hw]
         for i in range(1,hw):
-            img_c[-i,hw:-hw] = img[-1,:-hw]
+            img_c[-i,hw:-hw] = img[-i,:]
         img_c[-hw:,-hw:] = img[-1,-1]*img_c[-hw:,-hw:]
 
         #left and right block of the matrix
         img_c[hw:-hw,:hw] = img[:,:hw]
         img_c[hw:-hw,-hw:] = img[:,-hw:]
-    N,M = np.shape(img_c)
-    for i in range(hw,N-hw):
-        for j in range(hw,M-hw-1):
-                sub_matrix = img_c[i-hw:i+hw+1,j-hw:j+hw+1]
-                prod = np.multiply(sub_matrix,filter)
-                img_c[i,j] = np.average(prod)
-    return img_c[hw:-hw,hw:-hw]
+    return img_c
