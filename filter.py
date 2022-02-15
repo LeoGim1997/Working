fd1db62c3084904fbbb0fc9da6fe1d2a872b675e
LeoGim1997 <gimenezleo4@outlook.fr>
2022-02-15 10:45:10 +0000

    fix left-up margin issue in gaussian_b (filter.py)

--- filter.py
+++ filter.py
@@ -11,14 +11,14 @@ def gaussian_blur(img : np.array, sigma :float = 1 ,fill_pad = False) -> np.arra
     cut = np.shape(filter)
     hw = cut[0]//2
     #pad the image for fitting
-    img_c = np.zeros((n+2*hw,m+2*hw))
+    img_c = np.ones((n+2*hw,m+2*hw))
     img_c[hw:-hw,hw:-hw] = img
     #filling the 4 corner with the same corner value as the original image
     if fill_pad:
         #top block of the matrix
         img_c[0:hw,0:hw] = img[0,0]*img_c[0:hw,0:hw]
         for i in range(hw):
-            img_c[i,hw:-hw] = img[0,:-hw]
+            img_c[i,hw:-hw] = img[0,:]
         img_c[0:hw,-hw:] = img[0,-1]*img_c[0:hw,-hw:]
         #bottom block of the matrix
         img_c[-hw:,0:hw] = img[-1,0]*img_c[-hw:,0:hw]
@@ -27,8 +27,8 @@ def gaussian_blur(img : np.array, sigma :float = 1 ,fill_pad = False) -> np.arra
         img_c[-hw:,-hw:] = img[-1,-1]*img_c[-hw:,-hw:]
 
         #left and right block of the matrix
-        img_c[hw:,0:hw] = img[:,0:hw]
-        img_c[hw:,-hw:] = img[:,-hw:]
+        img_c[hw:-hw,:hw] = img[:,:hw]
+        img_c[hw:-hw,-hw:] = img[:,-hw:]
     N,M = np.shape(img_c)
     for i in range(hw,N-hw):
         for j in range(hw,M-hw-1):
