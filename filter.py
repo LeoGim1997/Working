305cd9c393ebd2f635d3be54e6a1f1e64251111e
LeoGim1997 <gimenezleo4@outlook.fr>
2022-02-15 10:36:00 +0000

    fix padding error in gaussian blur

--- filter.py
+++ filter.py
@@ -9,30 +9,30 @@ def gaussian_blur(img : np.array, sigma :float = 1 ,fill_pad = False) -> np.arra
     filter = gaussian_kernel(sigma)
     n,m = np.shape(img)
     cut = np.shape(filter)
-    half_witdh = cut[0]//2
+    hw = cut[0]//2
     #pad the image for fitting
-    img_c = np.zeros((n+half_witdh,m+half_witdh))
-    img_c[half_witdh:,half_witdh:] = img
+    img_c = np.zeros((n+2*hw,m+2*hw))
+    img_c[hw:-hw,hw:-hw] = img
     #filling the 4 corner with the same corner value as the original image
     if fill_pad:
         #top block of the matrix
-        img_c[0:half_witdh,0:half_witdh] = img[0,0]*img_c[0:half_witdh,0:half_witdh]
-        for i in range(half_witdh):
-            img_c[i,half_witdh:-half_witdh] = img[0,:-half_witdh]
-        img_c[0:half_witdh,-half_witdh:] = img[0,-1]*img_c[0:half_witdh,-half_witdh:]
+        img_c[0:hw,0:hw] = img[0,0]*img_c[0:hw,0:hw]
+        for i in range(hw):
+            img_c[i,hw:-hw] = img[0,:-hw]
+        img_c[0:hw,-hw:] = img[0,-1]*img_c[0:hw,-hw:]
         #bottom block of the matrix
-        img_c[-half_witdh:,0:half_witdh] = img[-1,0]*img_c[-half_witdh:,0:half_witdh]
-        for i in range(1,half_witdh):
-            img_c[-i,half_witdh:-half_witdh] = img[-1,:-half_witdh]
-        img_c[-half_witdh:,-half_witdh:] = img[-1,-1]*img_c[-half_witdh:,-half_witdh:]
+        img_c[-hw:,0:hw] = img[-1,0]*img_c[-hw:,0:hw]
+        for i in range(1,hw):
+            img_c[-i,hw:-hw] = img[-1,:-hw]
+        img_c[-hw:,-hw:] = img[-1,-1]*img_c[-hw:,-hw:]
 
         #left and right block of the matrix
-        img_c[half_witdh:,0:half_witdh] = img[:,0:half_witdh]
-        img_c[half_witdh:,-half_witdh:] = img[:,-half_witdh:]
-
-    for i in range(half_witdh,n-half_witdh):
-        for j in range(half_witdh,m-half_witdh-1):
-                sub_matrix = img_c[i-half_witdh:i+half_witdh+1,j-half_witdh:j+half_witdh+1]
+        img_c[hw:,0:hw] = img[:,0:hw]
+        img_c[hw:,-hw:] = img[:,-hw:]
+    N,M = np.shape(img_c)
+    for i in range(hw,N-hw):
+        for j in range(hw,M-hw-1):
+                sub_matrix = img_c[i-hw:i+hw+1,j-hw:j+hw+1]
                 prod = np.multiply(sub_matrix,filter)
                 img_c[i,j] = np.average(prod)
-    return img_c
+    return img_c[hw:-hw,hw:-hw]
