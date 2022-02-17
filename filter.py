ca1b609c754abdabbaa040070a8d09e0539f8f62
LeoGim1997 <gimenezleo4@outlook.fr>
2022-02-17 22:16:52 +0000

    add sobel filter function in filter.py

--- filter.py
+++ filter.py
@@ -1,6 +1,6 @@
 from function import gaussian_kernel, gaussian_kernel_basis
 import numpy as np
-
+from scipy.signal import convolve2d
 def gaussian_blur(img : np.array, sigma :float = 1 ,fill_pad = False) -> np.array:
     '''
     Apply a 7x7 gaussian blur on a img (default)
@@ -24,7 +24,8 @@ def gaussian_blur(img : np.array, sigma :float = 1 ,fill_pad = False) -> np.arra
 def image_padding(img : np.array , half_pad :int = 4,fill_pad =False) -> np.array:
     '''
     Function returning a padded image use for convolution
-    with a square fitler of half_with = half_pad
+    with a square fitler of half_with = half_pad.
+    The center of the image will be the orginal image
     '''
     n,m = np.shape(img)
     hw = half_pad
@@ -49,3 +50,62 @@ def image_padding(img : np.array , half_pad :int = 4,fill_pad =False) -> np.arra
         img_c[hw:-hw,:hw] = img[:,:hw]
         img_c[hw:-hw,-hw:] = img[:,-hw:]
     return img_c
+
+def sobel_filter_horizontal(img: np.array) -> np.array:
+    '''
+    Return the horizontal image gradient using Sobel convolution
+    '''
+    
+    filter = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+    Gx = image_padding(img,1,fill_pad=False)
+    img = np.zeros(np.shape(Gx))
+    N,M = np.shape(Gx)
+    for i in range(1,N-1):
+        for j in range(1,M-1):
+            sub_matrix = Gx[i-1:i+2,j-1:j+2]
+            prod = np.multiply(sub_matrix,filter)
+            img[i,j] = np.sum(prod)
+    return img[1:-1,1:-1]
+
+def sobel_filter_vertical(img: np.array) -> np.array:
+    '''
+    Return the vertical image gradient using Sobel convolution
+    '''
+
+    filter = np.array([[-1,-1,-1],[0,0,0],[1,1,1]])
+    Gy = image_padding(img,1,fill_pad=False)
+    img = np.zeros(np.shape(Gy))
+    N,M = np.shape(Gy)
+    for i in range(1,N-1):
+        for j in range(1,M-1):
+            sub_matrix = Gy[i-1:i+2,j-1:j+2]
+            img[i,j] = np.sum(np.multiply(sub_matrix,filter))
+    return img[1:-1,1:-1]
+
+def normalized_sobel_filter(img : np.array , threshold : int = 0) -> np.array:
+    Gx = sobel_filter_horizontal(img)
+    Gy = sobel_filter_vertical(img)
+    n,m = np.shape(img)
+    img_f = np.zeros((n,m))
+    img_f = np.abs(Gx)+np.abs(Gy)
+    if threshold != 0:
+        for i in range(n):
+            for j in range(m):
+                if img_f[i,j]<threshold:
+                    img_f[i,j] = 0
+                else:
+                    img_f[i,j] = 300
+    return img_f
+
+
+def Sobel_2(img: np.array) -> np.array:
+    a1 = np.matrix([1, 2, 1])
+    a2 = np.matrix([-1, 0, 1])
+    Kx = a1.T * a2
+    Ky = a2.T * a1
+
+    # Apply the Sobel operator
+    Gx = convolve2d(img, Kx, "same", "symm")
+    Gy = convolve2d(img, Ky, "same", "symm")
+    G = np.abs(Gx) + np.abs(Gy)
+    return G
