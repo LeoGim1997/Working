dcabef76239279e8b01f6a7c16829bc930114689
Leo_gim <48452355+LeoGim1997@users.noreply.github.com>
2022-04-30 18:47:24 +0200

    Pytest (#7)
    Add pytest env

---
+++ tests/test_noise.py
@@ -0,0 +1,14 @@
+import pytest
+import os
+from pathlib import Path
+import sys
+import numpy as np
+sys.path.append(Path(__file__).parents[1].as_posix())
+from confest import lena_standard
+from processing.noise import gaussian_noise, noise
+
+
+@pytest.mark.Noise
+def testGaussianNoise(lena_standard):
+    noise = gaussian_noise(img=lena_standard, mean=0, sigma=1)
+    assert np.shape(noise) == np.shape(lena_standard)
