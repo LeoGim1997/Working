6c7bd85c42897c07fed7422f101f68f72727c2ac
LeoGim1997 <gimenezleo4@outlook.fr>
2022-06-29 08:56:16 +0200

    add gaussian.py

---
+++ processing/gaussian.py
@@ -0,0 +1,48 @@
+import numpy as np
+from math import floor
+import matplotlib.pyplot as plt
+from im import MyImage
+from scipy import signal
+
+
+def gaussian_sample(sigma: float = 1,
+                    min: int = -3,
+                    max: int = 3,
+                    n_samples: int = 50) -> np.array:
+    '''
+    Return a 1-d Gaussian array of std sigma
+    '''
+    coeff = 1 / np.sqrt(sigma**2)
+    coeff_pi = coeff / np.sqrt(2 * np.pi)
+
+    x = np.linspace(min, max, n_samples)
+    x = coeff_pi * np.exp(-coeff * pow(x, 2))
+
+    return np.reshape(x, (len(x), 1))
+
+
+def gaussian_kernel(sigma: float = 1) -> np.array:
+    '''
+    Return normalized gaussian kernel of std sigma
+    of half-width h=3*sigma
+    '''
+    if sigma < 0:
+        raise ValueError('The std cannot be negative')
+    half_w = floor(sigma) * 3
+    witdh = 2 * half_w + 1
+    # add more point to the mesh-grid
+    x = gaussian_sample(sigma, -half_w, half_w, witdh * 5)
+    shape_x = np.shape(x)[0]
+    y = np.copy(x)
+    gaussian_mat = np.dot(x, np.transpose(x))
+    scale_factor = 1 / np.average(gaussian_mat)
+    gaussian_mat = scale_factor * gaussian_mat
+    # cropping the image to only get usefull value
+    n, m = np.shape(gaussian_mat)
+    center_x = witdh * 5 // 2
+    center_y = witdh * 5 // 2
+    cut = witdh * 5 // 5
+    kernel = gaussian_mat[center_x - cut - 1:center_x + cut, center_y - cut - 1:center_y + cut]
+    scale_factor = 1 / np.average(kernel)
+    kernel = scale_factor * kernel
+    return kernel
