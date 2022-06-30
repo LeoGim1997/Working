d877f49b6cd1e8703f48d5629c8f8bdc20b64b6b
LeoGim1997 <gimenezleo4@outlook.fr>
2022-06-30 17:37:53 +0200

    add multichannel handling

--- processing/kuwahara.py
+++ processing/kuwahara.py
@@ -18,10 +18,12 @@ MAP_DICT = {
 
 
 def generateQMatrix(mat: np.array):
+    """Allows to generate
+    """
     return (MAP_DICT.get(k)(mat) for k in range(1, 5))
 
 
-def kuwaharaResponse(mat: np.array) -> float:
+def filterResponse(mat: np.array) -> float:
     mat = np.asarray(mat)
     n, m = mat.shape
     if n != m:
@@ -29,22 +31,28 @@ def kuwaharaResponse(mat: np.array) -> float:
     if n == m != 5:
         raise ValueError(f'Input ndarray have wrong dim')
     stdlist = [np.std(m) for m in generateQMatrix(mat)]
-    max_sdt = np.argmax(stdlist)
+    max_sdt = np.argmin(stdlist)
     selectedQ = MAP_DICT.get(max_sdt + 1)
     return np.mean(selectedQ(mat))
 
 
 def kuwaharaConvolution(img: np.array, hp: int = 2) -> np.array:
-    p_img = image_padding(img, half_pad=hp, fill_pad=False)
+    p_img = image_padding(img, half_pad=hp, fill_pad=False, defaultfill=0)
     img = np.zeros(np.shape(p_img))
     N, M = np.shape(img)
     for i in range(hp, N - hp):
         for j in range(hp, M - hp):
             sub_matrix = p_img[i - hp:i + hp + 1, j - hp:j + hp + 1]
-            img[i, j] = kuwaharaResponse(sub_matrix)
+            img[i, j] = filterResponse(sub_matrix)
     return img[hp:-hp, hp:-hp]
 
 
-a = MyImage('lena').get_matrix('lena')
-b = kuwaharaConvolution(a)
-MyImage.show_compare(a, b)
+def kuwahara_response(img: np.array, hp: int = 2) -> np.array:
+    shape = img.shape
+    if len(shape) == 2:
+        return kuwaharaConvolution(img, hp=hp)
+    if len(shape) > 2:
+        a = np.copy(img)
+        for i in range(shape[-1]):
+            a[..., i] = kuwaharaConvolution(img=img[..., i], hp=hp)
+        return a
