d70f24ef6be48ac9c1c138f5309d6305b692bb3b
LeoGim1997 <gimenezleo4@outlook.fr>
2022-07-02 08:42:43 +0200

    implement haaris corner

--- processing/harrisCorner.py
+++ processing/harrisCorner.py
@@ -1,24 +1,8 @@
 import numpy as np
 from gradient_filtering import compute_gradient
-from im import MyImage
 from genericImage import opposite_eye
 from filter import image_padding
 import matplotlib.pyplot as plt
-from typing import Tuple
-from im import MyImage, normalize
-
-# 1 : Take the grayscale of the image
-# 2 : Apply gaussian fitler to smooth noise (Optional)
-# 3 : Apply Sobel operator to find x and y gradient
-# 4 : For each pixel, consider a 3*3 window around it
-# and compute the corner strength function
-# 5 : Find all pixels that exceed a certain threshold
-#  and are the local maxima within
-#   a certain window
-#   (to prevent redundant dupes of features).
-
-a = MyImage().get_matrix('chessboard')
-g, gx, gy = compute_gradient(a, operator='Sobel', return_xy_gradient=True)
 
 
 def haarisResponse(k: float = 0.004, num: int = 100,
@@ -52,20 +36,21 @@ def showGlobal_Response() -> None:
 def ellipse_Response(Ix: np.array, Iy: np.array,
                      k: float = 0.004) -> float:
 
-    a = np.sum(np.dot(Ix, Ix))
-    b = 2 * np.sum(np.dot(Ix, Iy))
-    c = np.sum(np.dot(Iy, Iy))
+    a = np.sum(np.multiply(Ix, Ix))
+    b = 2 * np.sum(np.multiply(Ix, Iy))
+    c = np.sum(np.multiply(Iy, Iy))
     l1 = 1 / 2 * (a + c + np.sqrt(b**2 + (a - c)**2))
     l2 = 1 / 2 * (a + c - np.sqrt(b**2 + (a - c)**2))
-    return l1 * l2 - k * (l1 + l2)**2
+    r = l1 * l2 - k * (l1 + l2)**2
+    return r
 
 
 def tensor_Response(Ix: np.array, Iy: np.array,
                     k: float = 0.04) -> float:
     # TODO : Fix the tensor
-    a = np.average(np.dot(Ix, Ix))
-    b = 2 * np.average(np.dot(Ix, Iy))
-    c = np.average(np.dot(Iy, Iy))
+    a = np.sum(np.multiply(Ix, Ix))
+    b = 2 * np.sum(np.multiply(Ix, Iy))
+    c = np.sum(np.multiply(Iy, Iy))
     M = np.asarray([[a, b], [b, c]])
 
     return np.linalg.det(M) - k * np.sum(np.diag(M))**2
@@ -86,10 +71,9 @@ def window_Response(Ix: np.array, Iy: np.array,
 def matrixResponse(img: np.array, k: float = 0.04,
                    hp: int = 1, method='ellipse') -> np.array:
     _, Gx, Gy = compute_gradient(img, operator='Sobel', return_xy_gradient=True)
-    Gx, Gy = Gx - np.mean(Gx), Gy - np.mean(Gy)
     Gx, Gy = image_padding(Gx, half_pad=hp), image_padding(Gy, half_pad=hp)
     n, m = Gx.shape
-    mat = np.copy(Gx)
+    mat = np.zeros((n, m))
     for i in range(hp, n - hp):
         for j in range(hp, m - hp):
             Ix = Gx[i - hp:i + hp + 1, j - hp:j + hp + 1]
@@ -108,12 +92,5 @@ def non_MaximalSupression(img: np.array, hp: int = 1) -> np.array:
             if (img[i, j] == np.max(submat)):
                 f[i, j] = img[i, j] * 5
             else:
-                f[i, j] = 0
+                f[i, j] = img[i, j] / 5
     return f
-
-
-a = MyImage('lena').get_matrix(fullpath='/Users/leogimenez/Desktop/git_depo_local/Working/image/image_folder/bbc-logo.jpeg')
-a = a[..., 0]
-a = normalize(a)
-b = matrixResponse(a, k=0.04, method='ellipse')
-MyImage.show(b, icmap='magma')
