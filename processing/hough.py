d77f4594fc5c20075fdf35d61683eb24d2013af2
LeoGim1997 <gimenezleo4@outlook.fr>
2022-07-10 11:10:32 +0100

    add hough function

--- processing/hough.py
+++ processing/hough.py
@@ -1,29 +1,37 @@
 import numpy as np
-from genericImage import straight_line
-import matplotlib.pyplot as plt
-maxTheta = 90
-minTheta = -maxTheta
-img = straight_line()
-n, m = img.shape
-rshape = max(n, m)
-theta = np.linspace(0, 180)
-accumulator = np.zeros((rshape + 2, theta.shape[0] + 2))
+from typing import Tuple
+from im import MyImage
 
-for i in range(n):
-    for j in range(m):
-        if img[i, j] == 0:
-            continue
-        for c, a in enumerate(theta):
-            r = i * np.cos(a) + j * np.sin(a)
-            if np.abs(r) > img.shape[0]:
-                r = img.shape[0] if r > 0 else -1 * img.shape[0]
-            accumulator[int(r) - 1, c] += 1
 
-h = accumulator
+def hough_line(img: np.array, theta=None) -> Tuple:
+    """
+    Compute the hough line detector inside for the
+    edges of the images.
+    """
+    if theta is None:
+        theta = np.linspace(-np.pi / 2, np.pi / 2, 360, endpoint=False)
 
-plt.figure()
-plt.imshow(np.log(1 + h),
-           extent=[theta[0], theta[-1],
-                   -150, 150],
-           cmap=plt.cm.gray, aspect=1 / 1.5)
-plt.show()
+    # Accumulator creation (Hough space)
+    offset = np.ceil(np.sqrt(img.shape[0]**2 + img.shape[1]**2))
+    max_distance = int(2 * offset) + 1
+    accum = np.zeros((max_distance, theta.shape[0]))
+    bins = np.linspace(-offset, offset, max_distance)
+
+    y_indx, x_indx = np.nonzero(img)
+    ndixs = y_indx.shape[0]
+    ntheta = theta.shape[0]
+
+    ctheta = np.cos(theta)
+    stheta = np.sin(theta)
+
+    for i in range(ndixs):
+        x, y = x_indx[i], y_indx[i]
+        for j in range(ntheta):
+            accum_idx = np.round(ctheta[j] * x + stheta[j] * y) + offset
+            accum[int(accum_idx), j] += 1
+    return accum, theta, bins
+
+
+def show_hough_space(img: np.array) -> None:
+    h, theta, bins = hough_line(img)
+    MyImage.show_compare(img, np.log(1 + h))
