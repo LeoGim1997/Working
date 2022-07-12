9098fd5d2ce5a82c1dd77da9dddf99a3eb6dcac1
LeoGim1997 <gimenezleo4@outlook.fr>
2022-07-12 09:09:28 +0100

    first method

---
+++ num/minmax.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Methode 1
+a = [0, 11, 4, 3, 8, 5, 6, 1, 7, 3, 10]
+
+k = 3
+g = a[0:k]
+d = a[len(a) - k:]
+g = g[::-1]
+d = g[::-1]
+b = g + a + d
+
+
+at = []
+c = int(k / 2)
+for i in range(k, len(b) - k):
+    t = b[i - c:i + c + 1]
+    at.append(max(t))
+at = np.array(at)
+print(f'methode1 = {at}')
+
+from scipy.ndimage import maximum_filter1d
+print(f'methode2 = {maximum_filter1d(a, size=k)}')
