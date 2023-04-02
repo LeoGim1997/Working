0ab132e76987d10264551feab5b163820572c43d
LeoGim1997 <gimenezleo4@outlook.fr>
2023-04-02 18:28:32 +0200

    add dithering.py

--- processing/dithering.py
+++ processing/dithering.py
@@ -1,44 +1,41 @@
 import numpy as np
-from im import MyImage
 import itertools as it
 from filter import image_padding
 
-# On load une image
-Img = MyImage("david")
-img = Img.get_matrix()
-img = img * 255
-print(img)
 
-
-def findcloset(pixel: float, color: float = 255):
-    return round(pixel / color)
-
-
-def dithering(img: np.array) -> np.array:
+def get_new_val(old_val: float, newmapping: np.array):
     """
-    Use the Floyd-Steinberg algorithm to dither an image.
+    Small function to map a pixel value inside
+    a new range of intensity value.
     """
-    hp = 1
-    imgc = np.copy(img)
-    imgprocess = image_padding(imgc, half_pad=hp, fill_pad=False)
-    n, m = np.shape(img)
-    arr = np.array(imgprocess, dtype=float) / 255
-    for i, j in it.product(range(n), range(m)):
-        old_pixel = imgprocess[i, j].copy()
-        new_pixel = findcloset(old_pixel)
-        imgprocess[i, j] = new_pixel
-        # we need to find the 2 local max inside
-        # the pixel neighborhood
+    idx = np.argmin(np.abs(newmapping - old_val), axis=0)
+    return newmapping[idx]
+
+
+def ditheringFS(img: np.array, nc) -> np.array:
+    """
+    Use the Floyd-Steinberg (FS) algorithm to dither an image.
+    The Algorithm of FS will act as follow:
+        For each pixel a new value of intensity will
+        be associated with a new value inside
+        the new color range.
+        The error will be propagated to the following pixel.
+    """
+    assert len(img.shape) == 2, f"Only n*p 1D chanel image are supported for now."
+    arr = image_padding(img, half_pad=1, fill_pad=False)
+    width, height = img.shape
+    maxitensite = np.max(img)
+    newmapping = np.linspace(0, maxitensite, nc)
+    for i, j in it.product(range(width), range(height)):
+        old_pixel = arr[i, j].copy()
+        new_pixel = get_new_val(old_pixel, newmapping)
+        arr[i, j] = new_pixel
         quant_error = old_pixel - new_pixel
-        imgprocess[i + 1, j] = imgprocess[i + 1, j] + quant_error * 7 / 16
-        imgprocess[i - 1, j + 1] = imgprocess[i - 1, j + 1] + quant_error * 3 / 16
-        imgprocess[i, j + 1] = imgprocess[i, j + 1] + quant_error * 5 / 16
-        imgprocess[i + 1, j + 1] = imgprocess[i + 1, j + 1] + quant_error * 1 / 16
-    return imgprocess[1:-1, 1:-1]
-
-
-# pixels[x + 1][y    ] := pixels[x + 1][y    ] + quant_error × 7 / 16
-# pixels[x - 1][y + 1] := pixels[x - 1][y + 1] + quant_error × 3 / 16
-# pixels[x    ][y + 1] := pixels[x    ][y + 1] + quant_error × 5 / 16
-# pixels[x + 1][y + 1] := pixels[x + 1][y + 1] + quant_error × 1 / 16
-MyImage.show_compare(img, dithering(img))
+        # Propagation of the quantification
+        # error to next pixels inside the current
+        # neigborhood.
+        arr[i, j + 1] += quant_error * 7 / 16
+        arr[i + 1, j - 1] += quant_error * 3 / 16
+        arr[i + 1, j] += quant_error * 5 / 16
+        arr[i + 1, j + 1] += quant_error * 1 / 16
+    return arr[1:-1, 1:-1]
