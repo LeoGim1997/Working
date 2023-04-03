3b7b3a9b3260de5a941e2102dc3f4aa0f2124926
LeoGim1997 <gimenezleo4@outlook.fr>
2023-04-03 13:48:10 +0200

    Add test for snakePath from dither

--- tests/test_dithering.py
+++ tests/test_dithering.py
@@ -8,7 +8,7 @@ from dithering import snakePath
 
 
 @pytest.mark.parametrize('n,m, gen',[(
-        2,2,((0,1),(-1,2))
+        2,2,((0,0),(0,1),(1,-1),(1,-2))
 )])
 def test_generator(n,m,gen):
     a = tuple(snakePath(n,m))
