773fc103dba83116a76601b5614d870e68ec28dd
LeoGim1997 <gimenezleo4@outlook.fr>
2023-04-30 12:45:59 +0200

    solve missing imports

--- processing/kuwahara.py
+++ processing/kuwahara.py
@@ -1,7 +1,7 @@
 import numpy as np
 from filter import image_padding
 from im import MyImage
-from typing import Iterator
+from typing import Iterator, Tuple
 
 
 def Q1(mat: np.ndarray) -> np.ndarray:
@@ -20,25 +20,18 @@ def Q4(mat: np.ndarray) -> np.ndarray:
     return mat[2:, 0:3]
 
 
-MAP_DICT = {1: Q1, 2: Q2, 3: Q3, 4: Q4}
-
-
-def generateQMatrix(mat: np.ndarray) -> Iterator:
-    """Allows to generate"""
-    return (MAP_DICT.get(k)(mat) for k in range(1, 5))
-
-
 def filterResponse(mat: np.ndarray) -> float:
+    mapDict = {1: Q1, 2: Q2, 3: Q3, 4: Q4}
     mat = np.asarray(mat)
     n, m = mat.shape
     if n != m:
         raise ValueError(f"Input ndarray is not square.")
     if n == m != 5:
         raise ValueError(f"Input ndarray have wrong dim")
-    stdlist = [np.std(m) for m in generateQMatrix(mat)]
+    stdlist = [np.std(m) for m in (Q1(mat), Q2(mat), Q3(mat), Q4(mat))]
     max_sdt = np.argmin(stdlist)
-    selectedQ = MAP_DICT.get(max_sdt + 1)
-    return np.mean(selectedQ(mat))
+    selectedQ = mapDict.get(max_sdt + 1)
+    return np.mean(selectedQ(mat),dtype=np.float16)
 
 
 def kuwaharaConvolution(img: np.ndarray, hp: int = 2) -> np.ndarray:
